7f3b30f828e742522db8400ecc0474096faedc1c
Chris <elorduychristian@gmail.com>
2025-10-07 12:44:05 +0200

    Agrege ejercicios.py con add

---
+++ ejercicios.py
@@ -0,0 +1,41 @@
+"""
+1.Muévete por los directorios de tu sistema y selecciona la carpeta en el que se hizo Hola Mundo 
+(Python, JavaScript), comprueba que estás dentro de esa carpeta, viendo la ruta que sigue el sistema.
+2. Configura tu nombre y email a nivel global
+- Comprueba que los tienes.
+- Inicia un repositorio en esa carpeta.
+- Averigua cómo ver el fichero oculto '.git'., renombra la rama principal a 'main' programacion.
+- Añade los archivos de Hola Mundo, todos al mismo tiempo.
+- Verifica que todo este correcto.
+3. Modifica el "¡Hola mundo!" de el enunciado y del ejercicio a ¡Hola Git!, añadelos y comentalo, en el mensaje explica cómo 
+hiciste este ejercicio, los anteriores, y la dificultad que te costó.
+4. En el segundo ejercicio de Hola Mundo, 'Debajo de ¡Hola Git!.
+- Borra el enunciado y el ejercicio, deja solo el número. 
+- Debajo de el dos,haz un reto de programación, terminalo, guardalo, pon la dificultad que te costó hacerlo.
+- Una vez terminado te das cuenta de que, no lo tenías que hacer aquí, vuelve a la versión anterior.
+- Deja el reto en espera con un mensaje cómo 'challenge: el nombre de el reto' dentro del commit de espera.   
+5. Crea un alias haciendo un bonito log.
+6. Crea un nuevo fichero 'hellogit.(py, js, etc...)'.
+- Comprueba el estado, crea un nuevo fichero de git para ignorar.
+- Vuelve a comprobar el estado.
+- Añade 'hellogit.(py, js, etc...)' dentro de ese fichero para ignorar.
+- Vuelve a comprobar el estado.
+- Trackea el fichero de ignorar.
+- Vuelve a comprobar el estado.
+- Comenta cómo lo hiciste y el modo en el que se incluye un fichero en ese fichero de ignorar.
+7. Crea un rama 'challenges' y cambia a esta rama, dentro de esta rama:
+- Restaura aquí el ejercicio en espera, añadelo y comenta el proceso a llegar y la dificultad.
+- Crea en una sola linea estos tres ficheros: roman_to_integer.(py, js, etc...) longest_word.(py, js, etc...) queue_simulator.(py, js, etc...)
+8. En cada uno de estos ficheros:
+- En el fichero 'roman_to_integer.(py, js, etc...)' en el que debes hacer un conversor de números romanos a enteros.
+- En el fichero 'longest_word.(py, js, etc...)' en el que se debe encontrar la palabra mas larga de una cadena de texto.
+- En el fichero 'queue_simulator.(py, js, etc...)' en el que se debe hacer un simulador de colas de espera.
+- Todos hechos, añadelos todos juntos de una vez, no de uno en uno, explica cómo lo hiciste y la dificultad de cada uno.
+9. Crea otro fichero con el reto que quieras, añadelo, ponte un cronometro 10 minutos.
+- Cuando el tiempo acabe paras de hacer el reto.
+- Cámbiate a la rama main, deja este fichero en espera.
+- Borra y haz el ejercicio 8 de Hola Mundo de nuevo.
+- comprueba la diferencia que hubo entre las dos versiones de ese ejercicio.
+- Vuelve a la rama 'challenges' recupera el reto que pusiste en espera y al terminarlo guardalo, explicalo y la dificultad que te supuso.
+10. Combina ambas ramas y añade tag(s) en donde creas que sea un punto de inflexion.
+"""
